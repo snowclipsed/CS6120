f8064f10244715b9dd420d04e0f5428205e0d4d4
snowclipsed <harrybishnoi@gmail.com>
2024-01-19 18:32:06 -0500

    Finished A0-A, Add : Aishwarya's Solution

---
+++ Assignments/A0/Solutions/NLP_A0_B/nlp_assignment_0/regex_extractor.py
@@ -0,0 +1,124 @@
+# Group J
+# Aishwarya Ravichandran
+# Hardik Bishnoi
+# B Dhanush Adhithya
+
+# assignment 0
+# importing pandas package to read .csv file
+# importing re package to call functions of regular expression
+
+# note -> install pandas in your virtual env for the program to run without roadblocks
+ 
+import pandas as pd
+import re
+
+# part a
+# extract() method
+
+def extract():
+
+    # reading user_details.csv file
+    user_df = pd.read_csv('C:/Users/harry/OneDrive/Documents/Northeastern University/CS6120/Assignments/A0/Solutions/NLP_A0_B/nlp_assignment_0/user_details.csv')
+
+    # printing user details as a data frame
+    print(user_df)
+
+    # iterating the user details data frame
+    for index, row in user_df.iterrows():
+
+        # re to extract the name attribute
+        full_name_match = re.search(r".*(?=:)", row["Description"])
+
+        # re to extract the address attribute
+        address_match = re.search(r"[0-9].*(St|Blvd)", row["Description"])
+
+        # re to extract the county attribute
+        county_match = re.search(r"[A-z]*(?= County)", row["Description"])
+
+        # re to extract the state attribute
+        state_match = re.search(r' [A-Z][A-Z](?=\.|,)', row["Description"])
+
+        # re to extract the phone attribute
+        phone_match = re.findall(r"[0-9]*-[0-9]*-[0-9]*", row["Description"])
+
+        # re to extract the name email id
+        email_match = re.search(r"[A-z0-9_]*@.*(.com|.org)", row["Description"])
+
+        # extracting fname and lname from name values using split() function and storing in first_name and last_name attribute
+        if full_name_match:
+            full_name = full_name_match.group().split(" ")
+            user_df.at[index, "first_name"] = full_name[0]
+            user_df.at[index, "last_name"] = full_name[1]
+
+        # getting address value and storing in address attribute
+        address = address_match.group() if address_match else ""
+        user_df.at[index, "address"] = address
+
+        # getting county value and storing in county attribute
+        county = county_match.group() if county_match else ""
+        user_df.at[index, "county"] = county
+
+        # getting state value and storing in state attribute
+        state = state_match.group() if state_match else ""
+        user_df.at[index, "state"] = state
+
+        # getting phone_number value and storing in phone# attribute
+        if phone_match:
+            for idx, phone_number in enumerate(phone_match):
+                user_df.at[index, f"phone{idx+1}"] = phone_number
+
+        # getting email value and storing in email attribute
+        email = email_match.group() if email_match else ""
+        user_df.at[index, "email"] = email
+
+    # printing the updated data frame
+    for index, row in user_df.iterrows():
+        print(row)
+
+    # Save the dataframe to a CSV file
+    user_df.to_csv('updated_user_details.csv', index=False)    
+
+# calling extract() method
+extract()
+
+# part b
+# read_book to read .txt files
+
+def read_book(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
+
+# searchText() method to search the respective text
+def searchText():
+
+    # to read pm vol1 and store it in pm_vol1 attribute
+    pm_vol1 = read_book('C:/Users/harry/OneDrive/Documents/Northeastern University/CS6120/Assignments/A0/Solutions/NLP_A0_B/nlp_assignment_0/practical_medicine_v1.txt')
+
+    # to read pm vol2 and store it in pm_vol2 attribute
+    pm_vol2 = read_book('C:/Users/harry/OneDrive/Documents/Northeastern University/CS6120/Assignments/A0/Solutions/NLP_A0_B/nlp_assignment_0/practical_medicine_v2.txt')
+
+    # to read pm vol3 and store it in pm_vol3 attribute
+    pm_vol3 = read_book('C:/Users/harry/OneDrive/Documents/Northeastern University/CS6120/Assignments/A0/Solutions/NLP_A0_B/nlp_assignment_0/practical_medicine_v3.txt')
+
+    # to read pm vol4 and store it in pm_vol4 attribute
+    pm_vol4 = read_book('C:/Users/harry/OneDrive/Documents/Northeastern University/CS6120/Assignments/A0/Solutions/NLP_A0_B/nlp_assignment_0/practical_medicine_v4.txt')
+
+    # to read pm vol5 and store it in pm_vol5 attribute
+    pm_vol5 = read_book('C:/Users/harry/OneDrive/Documents/Northeastern University/CS6120/Assignments/A0/Solutions/NLP_A0_B/nlp_assignment_0/practical_medicine_v5.txt')
+
+    # list of disease that has to be searched 
+    disease_list = ["Heart disease" , "Cancer" , "Stroke" , "Respiratory diseases" , "Alzheimer's disease" , "Diabetes" , "Influenza and Pneumonia" , "Kidney diseases" , 
+                   "Septicemia" , "Liver disease" , "Hypertension" , "Parkinson's disease" , "Chronic lower respiratory disease" , "Accidents/injuries" , "Osteoporosis" , "Asthma" , 
+                   "Depression" , "Oral health issues" , "HIV/AIDS" , "Tuberculosis" , "Malaria" , "Dengue fever" , "Hepatitis" , "Epilepsy" , "Multiple sclerosis"]
+
+    # iterating the disease list
+    for disease in disease_list:
+
+        # counting the occurrence of the disease ignoring case
+        occurrence = re.findall(disease, pm_vol1, re.IGNORECASE) + re.findall(disease, pm_vol2, re.IGNORECASE) + re.findall(disease, pm_vol3, re.IGNORECASE) + re.findall(disease, pm_vol4 , re.IGNORECASE) + re.findall(disease, pm_vol5, re.IGNORECASE)
+        
+        # printing the disease name and the corresponding occurrence value
+        print("Disease : " + disease + " | Occurrence : " + str(len(occurrence)))
+
+# calling the searchText() method
+searchText()
